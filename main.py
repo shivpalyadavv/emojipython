658d0e11fe3ac11f276a731af547f588bd7d907c
shivpalyadavv <shivpaly2@gmail.com>
2022-11-15 23:07:34 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import emoji
+
+print(emoji.emojize("Looking at you sparkling emerald eyes:face_blowing_a_kiss:"))
+print(emoji.emojize("It's the depth of your eyes that drowned my heart.:red_heart: Loving you was the best thing.:hibiscus:"))
